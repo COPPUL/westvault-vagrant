89bc40f97e52aaf041562046bb4b8cfde934bab2
Michael Joyce <ubermichael@gmail.com>
2017-08-24 12:42:08 -0700

    clean ups.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,25 @@
-#!/bin/bash
+!/bin/bash
 
 apt-get -y update
 apt-get -y upgrade
 
-# passwordless mysql root
+passwordless mysql root
 debconf-set-selections <<< "mysql-server mysql-server/root_password password \"''\""
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password \"''\""
 
-# basics.
+basics.
 apt-get -y install git vim wget curl emacs24-nox 
 
-# LAMP
+LAMP
 apt-get -y install apache2 php5 php5-dev php5-xsl php5-curl php5-cli php5-intl mysql-client mysql-server
 
-# staging server
+staging server
 apt-get -y install clamav clamav-daemon nodejs nodejs-dev npm php-pear
 
-curl -Ss https://getcomposer.org/installer | php
-mv composer.phar /usr/bin/composer
+freshclam
+/etc/init.d/clamav-daemon start
 
-npm install -g bower
-pear install Archive_Tar
+npm install --global --silent bower
+pear install Archive_Tar > /dev/null
+
+curl -Ss https://getcomposer.org/installer | php -- --quiet --install-dir=/usr/local/bin --filename=composer
