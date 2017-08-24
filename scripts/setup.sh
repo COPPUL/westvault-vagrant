cd4543b6c713b92aa8198e32796c2a63b6ec7eca
Michael Joyce <ubermichael@gmail.com>
2017-08-24 15:16:16 -0700

    more cleaning.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,18 +12,22 @@ apt-get install -y mysql-client mysql-server
 apt-get -y install git vim wget curl emacs24-nox gnupg
 
 # LAMP
-apt-get -y install apache2 php php-dev php-fxsl php-curl php-cli php-intl php-json php-mysql libapache2-mod-php 
+apt-get -y install apache2 php php-dev php-fxsl php-curl php-cli php-intl php-json php-mysql libapache2-mod-php php-pear
+pear install Archive_Tar > /dev/null
 
 # OwnCloud
 apt-get -y install libxml2-utils 
 
 # staging server
-apt-get -y install clamav clamav-daemon nodejs nodejs-dev npm php-pear
+apt-get -y install clamav clamav-daemon 
 
+# virus scanning
 freshclam
 /etc/init.d/clamav-daemon start
 
+# node/npm/bower
+apt-get -y install nodejs nodejs-dev npm
 npm install --global --silent bower
-pear install Archive_Tar > /dev/null
 
+# composer
 curl -Ss https://getcomposer.org/installer | php -- --quiet --install-dir=/usr/local/bin --filename=composer
