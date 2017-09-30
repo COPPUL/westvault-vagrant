fdf4caebed9368ebf6da6130218f719ed435753f
Michael Joyce <ubermichael@gmail.com>
2017-09-30 13:32:26 -0700

    ok sure that's important.

--- scripts/samba.sh
+++ scripts/samba.sh
@@ -2,7 +2,7 @@
 
 # https://www.howtogeek.com/howto/ubuntu/install-samba-server-on-ubuntu/
 
-apt-get install samba cifs-utils
+# apt-get install -y samba cifs-utils
 cp /vagrant/configs/smb.conf /etc/samba/smb.conf
 
 (echo vagrant; echo vagrant) | smbpasswd -s -a vagrant
