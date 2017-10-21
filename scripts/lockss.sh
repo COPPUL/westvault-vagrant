5e153448d0082976c08d05885821f3c16b9c7712
Michael Joyce <ubermichael@gmail.com>
2017-10-21 14:14:50 -0700

    closer to getting lockss working.

--- scripts/lockss.sh
+++ scripts/lockss.sh
@@ -5,16 +5,16 @@ export JAVA_HOME=/usr/lib/jvm/default-java
 
 pushd /var/www
 
-	git clone --branch release-candidate_1-73-b2 --depth 1 https://github.com/lockss/lockss-daemon.git
+	git clone --depth 1 https://github.com/lockss/lockss-daemon.git
 	
 	pushd lockss-daemon
  		ant btf
  		sudo chown -R vagrant:vagrant .
 		
- 		pushd test/frameworks/run_multiple_daemons
- 		cp /vagrant/configs/lockss/lockss.txt lockss.txt
- 		sudo chown -R vagrant:vagrant .
- 		sudo -u vagrant ./start
+#  		pushd test/frameworks/run_multiple_daemons
+#  		cp /vagrant/configs/lockss/lockss.txt lockss.txt
+#  		sudo chown -R vagrant:vagrant .
+#  		sudo -u vagrant ./start
 
 	popd
 
